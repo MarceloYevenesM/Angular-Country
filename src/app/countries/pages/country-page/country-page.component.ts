6b568537c91f668029d326698511dbe764407c33
MarceloYevenes <marcelo.yevenes.moreno@gmail.com>
2024-06-12 12:10:01 -0400

    Poder ver el país al apretar mas

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  constructor(private activatedRoute: ActivatedRoute, private countriesService: CountriesService, private router: Router) {
+
+  }
+
+  public country?: Country;
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params
+      .pipe(
+        /*  Recibe el valor anterior y regresa un nuevo observable */
+        switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id)), /* Suscrito a este resultado */
+      )
+      /* Hay que suscribirse para estar escuchando los cambios */
+      .subscribe((country) => {
+        if(!country) return this.router.navigateByUrl('');
+        return this.country = country;
+        return;
+      })
+  }
+
 
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -10,6 +10,19 @@ export class CountriesService {
   private apiKey: string = '38gLG1W5HBHoxavaPQfGpMb7XVnO0n2T'
 
   constructor(private http: HttpClient) { }
+
+
+  searchCountryByAlphaCode(code: string): Observable<Country | null> {
+    const params = new HttpParams()
+      .set('apikey', this.apiKey);
+    return this.http.get<Country[]>(`${this.apiUrl}/code/${code}`, { params })
+      .pipe(
+        /* se usa map por la api regresa un arreglo siempre */
+        map(countries => countries.length > 0 ? countries[0] : null),
+        /* Detectar o capturar el error y el of me permite construir otro observable */
+        catchError(error => of(null)) //Capturo y devuelvo un nuevo observable que es un arreglo vacio
+      );
+  }
 
 
   searchByCapital(term: string): Observable<Country[]> {
@@ -41,6 +54,7 @@ export class CountriesService {
         catchError(error => of([])) //Capturo y devuelvo un nuevo observable que es un arreglo vacio
       );
   }
+
 
 
 }
