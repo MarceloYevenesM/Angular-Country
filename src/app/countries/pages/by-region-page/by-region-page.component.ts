f96ec879de7e2e2a2c0e93da02fe325cd385a9b5
MarceloYevenes <marcelo.yevenes.moreno@gmail.com>
2024-06-13 15:19:35 -0400

    Mejorar las regiones

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+/* Uso types cuando se que no va extenderse */
+type Region = 'Africa'| 'Americas'| 'Asia'| 'Europe'| 'Oceania';
+
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,15 +14,18 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {
 
   }
 
-  searchByRegion(term: string): void {
+  searchByRegion(region: Region): void {
 
+    this.selectedRegion = region;
     /* Si no me suscribo nunca se emite */
-    this.countriesService.searchRegion(term).subscribe(
+    this.countriesService.searchRegion(region).subscribe(
       countries => {
         this.countries = countries;
       }
