59c2c06af24400fcc327c1e62b623bbf1bf7be11
MarceloYevenes <marcelo.yevenes.moreno@gmail.com>
2024-06-13 12:32:29 -0400

    Limpiar las suscripciones

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,11 +1,18 @@
-import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
+
+  /* Subject es un tipo especial de observable  */
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
+
   @Input()
   public placeholder = "";
 
@@ -13,9 +20,37 @@ export class SearchBoxComponent {
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+
+  ngOnInit(): void {
+
+    this.debouncerSuscription = this.debouncer
+    .pipe(
+      /* Si el usaurio deja de emitir valores por 1 segundo ahì llamo al suscribe */
+      debounceTime(300) //Cuanto tiempo esperar 1 segundo antes de hacer la emision
+    )
+    .subscribe(value => {
+      this.onDebounce.emit(value) //Emitir el debounce
+    });
+  }
+
+  /* Cuando el componente se destruye */
+  ngOnDestroy(): void {
+    /* Se implementa para suscripciones del onit excepto los services(https) */
+    this.debouncerSuscription?.unsubscribe(); //Romper las suscripciones para liberar memoria
+
+  }
+
   /* Metodo para recibir la info */
-  emitValue(value:string):void{
+  emitValue(value: string): void {
     this.onValue.emit(value); //Emito el valor capturado
+  }
+
+  onKeyPress(searchTerm: string) {
+    /* Emision segun el observable */
+    this.debouncer.next( searchTerm)
   }
 
 }
